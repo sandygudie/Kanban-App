71ab1d6fa325c5f0fed74a7e00628c972b99646c
sandygudie <goodnewssandy@gmail.com>
2023-12-16 01:09:40 +0100

    added marquee

--- cypress/e2e/index.cy.ts
+++ cypress/e2e/index.cy.ts
@@ -1,6 +1,6 @@
 describe("page load", () => {
   it("displays board", () => {
     cy.visit("/");
-    cy.contains("h1", "Effortlessly Manage your Projects.").should("be.visible");
+    cy.contains("p", "Effortlessly Manage").should("be.visible");
   });
 });
