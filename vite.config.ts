906237eff5c883d8ac4531cee6140d1f76b83e2a
sandygudie <goodnewssandy@gmail.com>
2023-12-16 15:26:58 +0100

    fixed deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,54 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
-import eslint from "vite-plugin-eslint";
-import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import eslint from 'vite-plugin-eslint'
 
 export default defineConfig({
   server: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: 3000,
   },
   plugins: [
     tsconfigPaths(),
     react(),
-    eslint({ cache: false }),
-    ViteImageOptimizer({
-      svg: {
-        multipass: true,
-        plugins: [
-          {
-            name: "preset-default",
-            params: {
-              overrides: {
-                cleanupNumericValues: false,
-                removeViewBox: false,
-                convertPathData: false,
-              },
-            },
-          },
-          "sortAttrs",
-          {
-            name: "addAttributesToSVGElement",
-            params: {
-              attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
-            },
-          },
-        ],
-      },
-      png: {
-        quality: 80,
-      },
-      jpeg: {
-        quality: 80,
-      },
-      jpg: {
-        quality: 80,
-      },
-
-      webp: {
-        lossless: true,
-      },
-    }),
-  ],
-});
+    eslint({ cache: false})
+  ]
+})
