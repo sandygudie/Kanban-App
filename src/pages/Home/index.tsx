d1c280c5d43e1fac67a6d2128b14293cb5e20380
sandygudie <goodnewssandy@gmail.com>
2023-12-18 17:55:38 +0100

    review landing pages

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,6 +5,7 @@ import Marquee from "react-fast-marquee";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { GoDotFill } from "react-icons/go";
 
 export default function Index() {
   gsap.registerPlugin(ScrollTrigger);
@@ -36,12 +37,6 @@ export default function Index() {
         delay: 1.2,
         y: 40,
       });
-      // gsap.to(".simplified-view", {
-      //   scrollTrigger: ".box",
-      //   start: "top top",
-      //   duration: 4,
-      //   x: 40,
-      // });
     },
 
     { scope: hero }
@@ -97,9 +92,9 @@ export default function Index() {
               </h1>
               <div className="description">
                 <p className="text-lg md:text-[25px] my-8 font-normal leading-[2.5rem]">
-                  Kanban offers a visual view for teams to manage tasks,
-                  quickly shift priorities, track project progress, and stay on
-                  top of deadlines.
+                  Kanban offers a visual view for teams to manage tasks, quickly
+                  shift priorities, track project progress, and stay on top of
+                  deadlines.
                 </p>
                 <div className="hover:scale-110 transition ease-in-out w-fit mx-auto delay-100 duration-300">
                   <Link
@@ -119,41 +114,65 @@ export default function Index() {
             <h1 className="text-3xl md:text-4xl font-bold">Features</h1>
 
             <div className="box my-6 md:my-16 ">
-              <div className="simplified-view flex items-center justify-center flex-col justify-between">
-                <h1 className="mb-8 text-xl md:text-2xl font-semibold">
-                  Simplified Board View
-                </h1>
-
+              <div className="simplified-view grid items-center md:grid-cols-2 md:ml-24">
                 <img
-                  src="simplified-board.svg"
-                  className="w-auto md:h-[22rem] mx-auto"
+                  src="full-board.webp"
+                  className="h-72 lg:h-[26rem]"
                   alt="mutiple-projects-image"
                 />
+                <div className="text-left md:px-8 py-4">
+                  <p className="text-2xl md:text-3xl font-semibold mb-4">
+                    What you get
+                  </p>
+                  <div>
+                    {[
+                      "Simplified board view.",
+                      "Handle multiple projects.",
+                      "Smooth drag and drop.",
+                      "Optimal layout on device's screen size.",
+                      "Create, read, update, and delete boards and tasks.",
+                      "Keep track of any changes.",
+                      "Toggle theme between dark and light modes.",
+                      "Form validations.",
+                    ].map((ele, index) => {
+                      return (
+                        <p
+                          key={index}
+                          className="text-black md:text-xl flex items-center gap-x-2 py-2"
+                        >
+                          {" "}
+                          <GoDotFill size={20} className="text-primary" />
+                          {ele}
+                        </p>
+                      );
+                    })}
+                  </div>
+                </div>
               </div>
-              <div className="flex items-center justify-center mt-20 gap-x-8 flex-wrap">
+              {/* <div className="flex items-center justify-center mt-20 gap-x-8 flex-wrap">
                 <div className="flex items-center justify-center flex-col justify-between">
-                  <h1 className="mb-8 text-xl md:text-2xl font-semibold">
+                  <h1 className="text-xl md:text-2xl font-semibold">
                     Handle Multiple Projects
                   </h1>
 
                   <img
-                    src="mutipleproject.svg"
+                    src="interactive.gif"
                     className="w-auto md:h-[22rem] mx-auto"
                     alt="mutiple-projects-image"
                   />
                 </div>
                 <div className="md:mt-0 flex items-center justify-center flex-col justify-between">
-                  <h1 className="mb-8 text-xl md:text-2xl font-semibold">
+                  <h1 className="text-xl md:text-2xl font-semibold">
                     Smooth Interaction
                   </h1>
 
                   <img
-                    src="interactiveproject.svg"
+                  src="interactive.gif"
                     className="w-auto md:h-[22rem] mx-auto"
                     alt="drag-and-drop-gif"
                   />
                 </div>
-              </div>
+              </div> */}
             </div>
           </section>
           <section
@@ -187,21 +206,49 @@ export default function Index() {
           </section>
           <section
             id="connect"
-            className="mt-16 md:mt-36 px-3 md:px-0 md:w-4/6 mx-auto py-16 text-center"
+            className="mt-16 bg-primary md:mt-36 py-16 text-center"
           >
-            <p className="text-xl mb-8">
-              {" "}
-              If you would like to add suggestions or corrections
-            </p>
-            <Link
-              to="/"
-              className="hover:bg-primary/20 font-bold bg-primary px-8 py-4 rounded-lg text-white"
-            >
-              {" "}
-              Connect on GitHub
-            </Link>
+            <p className="text-3xl mb-6 text-white font-bold"> Let&apos;s connect</p>
+            <div className="flex items-center flex-col text-sm text-white gap-y-2 justify-center">
+              <Link
+                target="_blank"
+                to="https://goodnewssandy.netlify.app/#project"
+                className="hover:text-gray"
+              >
+                {" "}
+                Portfolio
+              </Link>
+              <Link
+                to="https://github.com/sandygudie"
+                target="_blank"
+                className="hover:text-gray"
+              >
+                {" "}
+                Github
+              </Link>
+              <Link
+                target="_blank"
+                to="https://www.linkedin.com/in/goodnews-sandy"
+                className="hover:text-gray"
+              >
+                {" "}
+                LinkedIn
+              </Link>
+              <Link
+                target="_blank"
+                to="mailto:goodnewssandy@gmail.com"
+                className="hover:text-gray"
+              >
+                {" "}
+                goodnewssandy@gmail.com
+              </Link>
+            </div>
           </section>
+          
         </main>
+        <footer className="py-3 w-full text-sm bg-white text-center text-primary">
+            Copyright <span>&copy;</span>{new Date().getFullYear()}. Sandy Goodnews
+          </footer>
       </div>
     </div>
   );
