7a946d32bc9ae6de5eb970e0ed60842f9ca48ebc
sandygudie <goodnewssandy@gmail.com>
2023-12-23 13:35:32 +0100

    fixed routing

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React,{ useState, useEffect } from "react";
 import Header from "components/Header";
 import { Collapse } from "@chakra-ui/react";
 import { MdVisibilityOff } from "react-icons/md";
--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import { IoMdArrowBack } from "react-icons/io";
 
